07334f046e47689ae4eee223d2694647f2b594d7
clawboi <ethandawsonsound@gmail.com>
2026-02-13 20:10:57 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,25 @@
-window.addEventListener("load",()=>{
-  Engine.init();
-});
-window.__hideLoading?.();
+console.log("MAIN LOADED");
+
+window.onload = () => {
+  console.log("GAME START");
+
+  if (window.__hideLoading) {
+    window.__hideLoading();
+  }
+
+  const canvas = document.getElementById("game");
+  const ctx = canvas.getContext("2d");
+
+  function loop() {
+    ctx.fillStyle = "#141420";
+    ctx.fillRect(0,0,canvas.width,canvas.height);
+
+    ctx.fillStyle = "white";
+    ctx.font = "30px Arial";
+    ctx.fillText("Sophia Game Running", 250, 270);
+
+    requestAnimationFrame(loop);
+  }
+
+  loop();
+};
